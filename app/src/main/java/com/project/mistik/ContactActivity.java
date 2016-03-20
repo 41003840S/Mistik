138340317a0432c6e9c647b49c18b68915211273
Manuel <41003840s@iespoblenou.org>
2016-03-20 16:24:51 +0100

    Some Changes on ContactActivity

--- app/src/main/java/com/project/mistik/ContactActivity.java
+++ app/src/main/java/com/project/mistik/ContactActivity.java
@@ -1,12 +1,16 @@
 package com.project.mistik;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -18,6 +22,10 @@ import butterknife.ButterKnife;
 public class ContactActivity extends AppCompatActivity {
 
     @Bind(R.id.thumbnail) ImageView photo;
+    @Bind(R.id.cardTlf) CardView cardTlf;
+    @Bind(R.id.cardWeb) CardView cardWeb;
+    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,28 @@ public class ContactActivity extends AppCompatActivity {
                 RoundedBitmapDrawable circularBitmapDrawable = RoundedBitmapDrawableFactory.create(getBaseContext().getResources(), resource);
                 circularBitmapDrawable.setCircular(true);
                 photo.setImageDrawable(circularBitmapDrawable);
+            }
+        });
+
+        cardTlf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String PhoneNum = "+34-933-134-347";
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+Uri.encode(PhoneNum.trim())));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
+
+        cardWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.rotllantorra.com";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
             }
         });
 
